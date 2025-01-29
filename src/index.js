0eb3d4d3e51903c2c617eb65c6185e53e2cf139d
MiguelRony <miguel.rony@icloud.com>
2025-01-28 23:18:55 -0600

    add basic skeleton and start header styling

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import styles from './styles.css';
+import dogImg from './utils/images/dog.jpg';
+
+const header = document.querySelector('header');
+const img = header.querySelector('img');
+
+img.src = dogImg;
